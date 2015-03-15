1d338917a02883ef112f4993ca19f896b66415e9
filipevalentin <filipethesnake2@gmail.com>
2015-03-15 15:27:37 -0300

    Adicionando Note do Fernando a lista local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Filipe-PC', 'Filipe-Note'),
+	'local' => array('Filipe-PC', 'Filipe-Note', 'FernandoMelo-PC'),
 
 ));
 
