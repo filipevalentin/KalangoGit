008e120e792f07e163211f1745db52977c3ad14b
filipevalentin <filipethesnake2@gmail.com>
2015-03-15 01:04:58 -0300

    Alterações de e-mail, config/email

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'kalangogame@gmail.com', 'name' => 'KalanGO'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'kalangogame@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => 'Lagarto2014',
 
 	/*
 	|--------------------------------------------------------------------------
