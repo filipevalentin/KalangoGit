617889824cd11ea6678dabcff055b2c286d7fe5d
fernandomelo2007 <touch2008>
2015-03-21 20:30:57 -0300

    Ajustes de Layout - 7

--- app/views/professor/showAdmin.blade.php
+++ app/views/professor/showAdmin.blade.php
@@ -64,7 +64,7 @@
     <h1>Gerenciar Professores</h1>
     <ol class="breadcrumb">
         <li><a href="#" ><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Widgets</li>
+        <li class="active">Gerenciar Professores</li>
     </ol>
 </section>
 
