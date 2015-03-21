e01b13bd6d6d29918ddd23a7a46dbb8b72673787
filipevalentin <filipethesnake2@gmail.com>
2015-03-21 18:25:06 -0300

    Atualização Para novo Banco: Skills -> Topicos

---
+++ Alterações-Topico+DificuldadePontos-Questao.php
@@ -0,0 +1,75 @@
+Alterações mecessárias:
+
+<h1>
+
+	<p>Administrador</p>
+	<li>
+		<li>Atividade - Questoes</li>
+			<div name= "Página editarAdmin">
+
+				#View-Layout
+					- adicionar uma tag ao lado esquerdo do "tool-box" de botões com uma tag indicando o nome do tópico 
+					  relativo a questão
+
+				#Cadastro
+					- atualizar modal com novos campos: "select-dificuldade" e "select-tópico"
+					- atualizar rota pra setar a dificuldade
+
+				#Update
+					- atualizar modal com novos campos
+					- atualizar botão "lápis" com attr dificuldade e tópico
+					- atualizar javascript
+
+			</div>
+			
+		<li>Atividade Extra - Questoes</li>
+			<div name= "Página extraEditarAdmin">
+
+				#Cadastro
+					- atualizar modal com novos campos: "select-dificuldade" e "select-tópico"
+					- atualizar rota pra setar a dificuldade
+
+				#Update
+					- atualizar modal com novos campos
+					- atualizar botão "lápis" com attr dificuldade e tópico
+					- atualizar javascript
+
+			</div>
+	</li>
+	
+	<p>professor</p>
+	<li>
+		<li>Atividade - Questoes</li>
+			<div name= "Página professorView e professorTurmaView">
+
+				#View-Layout
+					- adicionar uma tag ao lado esquerdo do "tool-box" de botões com uma tag indicando o nome do tópico 
+					  relativo a questão
+
+		<li>Atividade Extra - Questoes</li>
+			<div name= "Página editarAdmin">
+
+				#Display-Layout
+					- adicionar uma tag ao lado esquerdo do "tool-box" de botões com uma tag indicando o nome do tópico 
+					  relativo a questão
+
+				#Cadastro
+					- atualizar modal com novos campos: "select-dificuldade" e "select-tópico"
+					- atualizar rota pra setar a dificuldade
+
+				#Update
+					- atualizar modal com novos campos
+					- atualizar botão "lápis" com attr dificuldade e tópico
+					- atualizar javascript
+
+			</div>
+	</li>
+
+	<p>Aluno</p>
+		<li>Responder Atividade</li>
+			<div name="Pagina responderAtividade">
+				#View
+					- Adicionar uma tag com  nome do tópico relacionado
+			</div>
+
+</h1>
