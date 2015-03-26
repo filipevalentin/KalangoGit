9a4624f17e02380d42e727936974473e174fcf41
filipevalentin <filipethesnake2@gmail.com>
2015-03-26 01:03:03 -0300

    Revert "erro"
    This reverts commit 035e6708a4200eafe6b8c1605412c6b9ec2293df.

---
+++ app/models/Modulo.php
@@ -0,0 +1,32 @@
+<?php 
+	class Modulo extends Eloquent{
+
+		public $timestamps = false;
+
+		public function turmas(){
+        	return $this->hasMany('Turma', 'idModulo');
+   		}
+
+   		public function curso(){
+        	return $this->belongsTo('Curso', 'idCurso');
+        }
+
+        public function aulas(){
+			return $this-> hasMany('Aula','idModulo');
+		}
+
+		public function atividades(){
+			return $this-> hasManyThrough('Atividade','Aula', 'idModulo', 'idAula');
+		}
+
+		public function questoes(){
+			return $this->hasManyThrough('Questao', 'Atividade', 'idModulo', 'idAtividade');
+		}
+
+		public function alunos(){
+			return $this->hasManyThrough('Aluno', 'Turma', 'idModulo', 'idTurma');
+		}
+
+	}
+
+ ?>
