f038a3cfb6d640be20dc8ac8a1c9e23de620de77
filipevalentin <filipethesnake2@gmail.com>
2015-03-27 03:51:21 -0300

    Update- CRud Idioma

--- app/views/idioma/show.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>{{ $categorias->name }}</h1>
-
-<div class="jumbotron text-center">
-	<h2>{{ $categorias->name }}</h2>
-	<p>
-		<strong>Nome:</strong> {{ $categorias->nome }}<br>
-
-	</p>
-</div>
