8c26d008a1d6d8431cd099c02c74ce1146baafa8
filipevalentin <filipethesnake2@gmail.com>
2015-04-02 01:05:02 -0300

    go back

--- app/views/master-admin.blade.php
+++ app/views/master-admin.blade.php
@@ -268,7 +268,7 @@
         <!-- jQuery 2.1.3 -->
         <script src="/plugins/jQuery/jQuery-2.1.3.min.js"></script>
         <!-- jQuery UI 1.11.2 -->
-        <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
+        <script src="/js/jquery-ui.min.js" type="text/javascript"></script>
         <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
         <script>
           $.widget.bridge('uibutton', $.ui.button);
@@ -280,7 +280,7 @@
         <!-- Bootstrap WYSIHTML5 -->
         <script src="/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
         <!-- Data Tables -->
-        <script src="http://cdn.datatables.net/1.10.5/js/jquery.dataTables.min.js" type="text/javascript"></script>
+        <script src="/plugins/datatables/jquery.dataTables.min.js" type="text/javascript"></script>
         <!-- AdminLTE App -->
         <script src="/dist/js/app.min.js" type="text/javascript"></script>
 
