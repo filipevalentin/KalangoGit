4892f30c87a80d15af97376a787e41017bd3a72f
fernandomelo2007 <touch2008>
2015-04-02 23:29:37 -0300

    Ajustes de Layout 02/04/15 - 4

--- app/views/aviso/showAdmin.blade.php
+++ app/views/aviso/showAdmin.blade.php
@@ -3,10 +3,10 @@
 @section('maincontent')
 
 	<section class="content-header">
-		<h1>Aviso - {{$aviso->titulo}}</h1>
+		<h1>Avisos - {{$aviso->titulo}}</h1>
 	    <ol class="breadcrumb">
 	        <li><a href="#" ><i class="fa fa-dashboard"></i> Home</a></li>
-	        <li class="active">Avisos</li>
+	        <li class="active">Avisos - {{$aviso->titulo}}</li>
 	    </ol>
 	</section>
 
