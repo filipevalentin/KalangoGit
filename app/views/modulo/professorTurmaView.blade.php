c3c65510a728ac9ae963d71a965c30b27e1a6b77
fernandomelo2007 <touch2008>
2015-04-03 00:12:34 -0300

    Ajustes de Layout 02/04/15 - 16

--- app/views/modulo/professorTurmaView.blade.php
+++ app/views/modulo/professorTurmaView.blade.php
@@ -41,10 +41,11 @@
 
 @section('maincontent')
     <section class="content-header">
-        <h1>Gerenciar Turmas - Inglês Kids</h1>
+        <h1>Gerenciar Turmas</h1>
         <ol class="breadcrumb">
             <li><a href="#" ><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Widgets</li>
+            <li class="active">Gerenciar Cursos</li>
+			<li class="active">Gerenciar Turmas</li>
         </ol>
     </section>
 
