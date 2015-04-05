d52aad5f2dfe2e0351c57cd89ce1bc1760c02205
fernandomelo2007 <touch2008>
2015-04-05 15:18:33 -0300

    Ajustes nas validações - 1

--- app/views/administrador/editarAdmin.blade.php
+++ app/views/administrador/editarAdmin.blade.php
@@ -130,15 +130,15 @@
                                                 <div class="col-sm-9 col-xs-12">
                                                     <input type="text" name="id" class="form-control" style="display:none;" value={{$administrador->id}}>
 													<div id="div_nome" class="form-group margin">
-                                                        <label class="control-label" for="nome"><i id="icone_nome" class="fa"></i></label> <b>Nome</b>
+                                                        <label class="control-label" for="nome"><i id="icone_nome" class="fa"></i><b> Nome</b></label>
                                                         <input type="text" autocomplete="off" name="nome" onblur="fcn_recarregaCores();" maxlength="50" class="form-control somenteLetras nomeObrigatorio" value={{$administrador->nome}}>
                                                     </div>
                                                     <div id="div_sobrenome" class="form-group margin">
-                                                        <label class="control-label" for="sobrenome"><i id="icone_sobrenome" class="fa"></i></label> <b>Sobrenome</b>
+                                                        <label class="control-label" for="sobrenome"><i id="icone_sobrenome" class="fa"></i><b> Sobrenome</b></label>
                                                         <input type="text" autocomplete="off" name="sobrenome" onblur="fcn_recarregaCores();" maxlength="50" class="form-control somenteLetras sobrenomeObrigatorio" value={{$administrador->sobrenome}}>
                                                     </div>
                                                     <div id="div_email" class="form-group margin">
-                                                        <label class="control-label" for="email"><i id="icone_email" class="fa"></i></label> <b>E-mail</b>
+                                                        <label class="control-label" for="email"><i id="icone_email" class="fa"></i><b> E-mail</b></label>
                                                         <input type="text" autocomplete="off" maxlength="50" name="email" class="form-control emailObrigatorio" onblur="fcn_recarregaCores();fcn_validaEmail(this);" value={{$administrador->email}}>
                                                     </div>
 													<div class="margin" style="padding-bottom:10px;">
@@ -166,15 +166,15 @@
                                             </div>
 											<div class="col-md-9 col-sm-9 col-xs-12">
 												<div id="div_senhaAtual" class="form-group margin">
-													<label class="control-label" for="inputSuccess"><i id="icone_senhaAtual" class="fa"></i></label> <b>Senha Atual</b>
+													<label class="control-label" for="inputSuccess"><i id="icone_senhaAtual" class="fa"></i><b> Senha Atual</b></label>
 													<input type="password" maxlength="12" class="form-control senhaAtualObrigatoria" name="senhaAtual" id="senhaAtual" onblur="fcn_recarregaCoresSenha();fcn_validaSenhaAtual(6, 12, this.value);" >
 												</div>
 												<div id="div_novaSenha" class="form-group margin">
-													<label class="control-label" for="inputSuccess"><i id="icone_novaSenha" class="fa"></i></label> <b>Nova Senha</b>
+													<label class="control-label" for="inputSuccess"><i id="icone_novaSenha" class="fa"></i><b> Nova Senha</b></label>
 													<input type="password" maxlength="12" class="form-control novaSenhaObrigatoria" name="senha" id="senha" onblur="fcn_recarregaCoresSenha();fcn_validaNovaSenha(6, 12, this.value);" >
 												</div>
 												<div id="div_confirmarNovaSenha" class="form-group margin">
-													<label class="control-label" for="inputSuccess"><i id="icone_confirmarNovaSenha" class="fa"></i></label> <b>Confirmar Nova Senha</b>
+													<label class="control-label" for="inputSuccess"><i id="icone_confirmarNovaSenha" class="fa"></i><b> Confirmar Nova Senha</b></label>
 													<input type="password" maxlength="12" class="form-control confirmaNovaSenhaObrigatoria" name="novaSenha" id="novaSenha" onblur="fcn_recarregaCoresSenha();fcn_validaConfirmaNovaSenha(6, 12, this.value);" >
 												</div>
 												<div class="margin" style="padding-bottom:10px;">
