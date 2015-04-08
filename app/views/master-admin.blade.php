247739be6312a43b7ae775e81863b7bef95a6ef6
filipevalentin <filipethesnake2@gmail.com>
2015-04-07 21:34:16 -0300

    fix teste reconhecimento de voz

--- app/views/master-admin.blade.php
+++ app/views/master-admin.blade.php
@@ -25,7 +25,7 @@
         <link rel="stylesheet" href="/css/dataTables.tableTools.css" type="text/css">
         <link href="/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
 		<link rel="shortcut icon" href="../img/favicon.ico">
-        
+        @yield('css')
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
