7aae4c67aebc750fc66bf936e9bdaaa1f278e721
fernandomelo2007 <touch2008>
2015-04-22 01:33:30 -0300

    Ajustes de Layout - 14

--- app/views/propaganda/showAdmin.blade.php
+++ app/views/propaganda/showAdmin.blade.php
@@ -105,7 +105,7 @@
 	<section class="content-header">
 	    <h1>Propagandas</h1>
 	    <ol class="breadcrumb">
-	        <li><a href="#" ><i class="fa fa-dashboard"></i> Home</a></li>
+	        <li><a href="/admin/home" ><i class="fa fa-dashboard"></i> Home</a></li>
 	        <li class="active">Propagandas</li>
 	    </ol>
 	</section>
