d47e3212916c1b9de5e69285d4eb63fdc80da0a7
fernandomelo2007 <touch2008>
2015-04-22 01:14:04 -0300

    Ajustes de Layout - 6

--- app/views/idioma/adminView.blade.php
+++ app/views/idioma/adminView.blade.php
@@ -57,7 +57,7 @@
 	<section class="content-header">
 	    <h1>Gerenciar Idiomas</h1>
 	    <ol class="breadcrumb">
-	        <li><a href="#" ><i class="fa fa-dashboard"></i> Home</a></li>
+	        <li><a href="/admin/home" ><i class="fa fa-dashboard"></i> Home</a></li>
 	        <li class="active">Gerenciar Idiomas</li>
 	    </ol>
 	</section>
