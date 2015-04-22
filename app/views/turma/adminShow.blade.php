41f48e5032f933b38993dc25c965d4f126558608
fernandomelo2007 <touch2008>
2015-04-22 01:14:24 -0300

    Ajustes de Layout - 9

--- app/views/turma/adminShow.blade.php
+++ app/views/turma/adminShow.blade.php
@@ -111,7 +111,7 @@
 <section class="content-header">
     <h1>Gerenciar Turmas</h1>
     <ol class="breadcrumb">
-        <li><a href="/" ><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="/admin/home" ><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Gerenciar Turmas</li>
     </ol>
 </section>
