b4f36cc3a660a8da8c610ffb5a5f48fc72b7ccf5
filipevalentin <filipethesnake2@gmail.com>
2015-05-03 20:35:53 -0300

    Alteração Calendário

--- app/views/aluno/editarAdmin.blade.php
+++ app/views/aluno/editarAdmin.blade.php
@@ -15,7 +15,9 @@
 		monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
 		monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
 		nextText: 'Próximo',
-		prevText: 'Anterior'
+		prevText: 'Anterior',
+        changeMonth: true,
+        changeYear: true
 	});
   });
 </script>
