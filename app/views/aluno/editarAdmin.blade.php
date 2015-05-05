d073b526b6a35ddfee44f62b421641201636f2a1
fernandomelo2007 <touch2008>
2015-05-05 01:17:32 -0300

    Padronização dos datepickers - 1

--- app/views/aluno/editarAdmin.blade.php
+++ app/views/aluno/editarAdmin.blade.php
@@ -201,7 +201,7 @@
                                                     </div>
                                                     <div id="div_dataNascimento" class="form-group margin">
                                                          <label class="control-label" for="dataNascimento"><i id="icone_dataNascimento" class="fa"></i> Data de Nascimento</label>
-                                                        <input type="text" autocomplete="off" name="dataNascimento" id="dataNascimento" onblur="fcn_recarregaCores();" class="form-control validaData dataNascimentoObrigatorio" value={{$aluno->dataNascimento}}>
+                                                        <input readonly type="text" autocomplete="off" name="dataNascimento" id="dataNascimento" onblur="fcn_recarregaCores();" class="form-control validaData dataNascimentoObrigatorio" value={{$aluno->dataNascimento}}>
                                                     </div>
                                                     <div id="div_email" class="form-group margin">
                                                         <label class="control-label" for="email"><i id="icone_email" class="fa"></i> E-mail</label>
