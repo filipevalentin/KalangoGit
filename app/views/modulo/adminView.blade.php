83847187f72eb287a21d08782d66d5a67b25e805
filipevalentin <filipethesnake2@gmail.com>
2015-05-05 01:13:57 -0300

    fix

--- app/views/modulo/adminView.blade.php
+++ app/views/modulo/adminView.blade.php
@@ -301,14 +301,6 @@
                                                 </div>
                                             @endif
 
-                                            
-                                            
-                                            @if($aula->materialApoio->count() == 0 && $aula->atividades->count() == 0 )
-                                                <div class="callout callout-danger" style="max-width:50%; margin:auto;">
-                                                    <h4 class="center">Nenhum conteúdo de aula</h4>
-                                                </div>
-                                            @endif
-
                                             @foreach ($aula->materialApoio as $material)
                                                 <div class="alert alert-success alert-dismissable" style="min-height: 55px;">
                                                     <i class="fa  fa-check-circle" style="left: -15px; top: 7px;"></i>
