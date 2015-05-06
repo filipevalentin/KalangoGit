47146848676b9f266b03753e1780e7bbae04369e
fernandomelo2007 <touch2008>
2015-05-06 00:59:12 -0300

    Retirada do Caminho de Pão - 1

--- app/views/administrador/home.blade.php
+++ app/views/administrador/home.blade.php
@@ -216,10 +216,10 @@
 
 <section class="content-header">
     <h1>Gerenciar Cursos</h1>
-    <ol class="breadcrumb">
+    <!--<ol class="breadcrumb">
         <li><a href="/admin/home" ><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Gerenciar Cursos</li>
-    </ol>
+    </ol>-->
 </section>
 
 <section class="content">
