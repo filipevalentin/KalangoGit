1f4d71a6f66956755b39f96a4a3199dc4fadcc4c
fernandomelo2007 <touch2008>
2015-05-15 20:56:12 -0300

    Ajuste Validação - 5

--- app/views/turma/adminShow.blade.php
+++ app/views/turma/adminShow.blade.php
@@ -9,6 +9,7 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="exampleModalLabel">Editar Turma</h4>
             </div>
+			
             <div class="modal-body">
                 <form method="POST" action="/admin/atualizarTurma">
                     <div class="form-group">
