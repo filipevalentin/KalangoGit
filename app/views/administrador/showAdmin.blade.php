e7782487f723388dd11e4527f62d63cbc7681498
fernandomelo2007 <touch2008>
2015-05-17 17:51:51 -0300

    Ajuste de Autocomplete - 1

--- app/views/administrador/showAdmin.blade.php
+++ app/views/administrador/showAdmin.blade.php
@@ -36,6 +36,11 @@
                     </div>
                     <div id="div_senha" class="form-group">
 						<label class="control-label" for="senha"><i id="icone_senha" class="fa"></i> Senha</label>
+						
+						<!-- Campo fake para enganar o autocomplete do chrome -->
+						<input type="password" name="password" style="display:none;" >
+						<!------------------------------------------------------->
+						
 						<input type="password" autocomplete="off" id="senha" name="password" maxlength="12" class="form-control senhaObrigatoria" onblur="fcn_recarregaCores();fcn_validaSenha(6, 12, this.value);" >
 					</div>	
 					<div id="div_confirmaSenha" class="form-group">
