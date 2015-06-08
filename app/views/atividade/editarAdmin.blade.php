589e70bd223b1a9521bbba87c30dcfea9b6bfad9
filipevalentin <filipethesnake2@gmail.com>
2015-06-08 13:56:37 -0300

    Revert "Fix pontuação"
    This reverts commit cb1eb720935cb856fd1d174da3e185bf816426f9.

--- app/views/atividade/editarAdmin.blade.php
+++ app/views/atividade/editarAdmin.blade.php
@@ -405,8 +405,8 @@
                                             <button class="btn btn-success btn-sm" data-toggle="modal" rel="tooltip" data-placement="left" title="Editar Questão" data-target="#editarme" data-id="{{$questao->id}}" data-enunciado="{{$questao->enunciado}}" data-categoria="{{$questao->categoria}}" data-a="{{$questao->alternativaA}}" data-b="{{$questao->alternativaB}}" data-c="{{$questao->alternativaC}}" data-d="{{$questao->alternativaD}}" data-respostacerta="{{$questao->respostaCerta}}" data-numero="{{$questao->numero}}" data-tipo="me" data-topico="{{$questao->topico->id}}" data-dificuldade="{{$questao->pontos}}"><i class="fa fa-pencil"></i></button>
                                            <a href="/admin/questao/deletar/{{$questao->id}}"><button class="btn btn-danger btn-sm btn_deletar_questao"><i class="fa fa-times"></i></button></a>
                                         </div>
-                                        <small class="badge pull-right bg-green" style="margin: 0px 6px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
-		                                        	<small class="badge pull-right bg-red" style="margin: 0px 6px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
+                                        <small class="badge pull-right bg-green" style="margin: 0px 76px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
+		                                        	<small class="badge pull-right bg-red" style="margin: 0px 76px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
                                     </div>
                                     <div id="ME{{$questao->id}}" class="panel-collapse collapse">
                                         <div class="box-body">
@@ -574,10 +574,10 @@
                                             <a href="/admin/questao/deletar/{{$questao->id}}"><button class="btn btn-danger btn-sm btn_deletar_questao"><i class="fa fa-times"></i></button></a>
                                         </div>
                                         @if(substr($questao->categoria,1) == "4")
-                                            <small class="badge pull-right bg-red" style="margin: 0px 6px 0px 5px;"><i class="fa fa-microphone"></i>Rec. de Voz</small>
+                                            <small class="badge pull-right bg-red" style="margin: 0px 76px 0px 5px;"><i class="fa fa-microphone"></i>Rec. de Voz</small>
                                         @endif
-                                        <small class="badge pull-right bg-green" style="margin: 0px 6px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
-		                                        	<small class="badge pull-right bg-red" style="margin: 0px 6px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
+                                        <small class="badge pull-right bg-green" style="margin: 0px 76px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
+		                                        	<small class="badge pull-right bg-red" style="margin: 0px 76px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
                                     </div>
                                     <div id="RU{{$questao->id}}" class="panel-collapse collapse">
                                         <div class="box-body">
--- app/views/atividade/professorView.blade.php
+++ app/views/atividade/professorView.blade.php
@@ -52,8 +52,8 @@
                                         <h4 class="box-title">
                                             <a data-toggle="collapse" data-parent="#accordion" href="#ME{{$questao->id}}" class="">Questão {{$aux++}}</a>
                                         </h4>
-                                        <small class="badge pull-right bg-green" style="margin: 0px 6px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
-		                                        	<small class="badge pull-right bg-red" style="margin: 0px 6px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
+                                        <small class="badge pull-right bg-green" style="margin: 0px 76px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
+		                                        	<small class="badge pull-right bg-red" style="margin: 0px 76px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
                                     </div>
                                     <div id="ME{{$questao->id}}" class="panel-collapse collapse">
                                         <div class="box-body">
@@ -217,10 +217,10 @@
                                             <a data-toggle="collapse" data-parent="#accordion" href="#RU{{$questao->id}}" class="">Questão {{$aux++}}</a>
                                         </h4>
                                         @if(substr($questao->categoria,1) == "4")
-                                            <small class="badge pull-right bg-red" style="margin: 0px 6px 0px 5px;">Rec. de Voz</small>
+                                            <small class="badge pull-right bg-red" style="margin: 0px 76px 0px 5px;">Rec. de Voz</small>
                                         @endif
-                                        <small class="badge pull-right bg-green" style="margin: 0px 6px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
-		                                        	<small class="badge pull-right bg-red" style="margin: 0px 6px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
+                                        <small class="badge pull-right bg-green" style="margin: 0px 76px 0px 5px;"><?php if($questao->topico->nome != null) echo $questao->topico->nome ?></small>
+		                                        	<small class="badge pull-right bg-red" style="margin: 0px 76px 0px 5px;"><?php if($questao->pontos != null) echo $questao->pontos ?> pontos</small>
                                     </div>
                                     <div id="RU{{$questao->id}}" class="panel-collapse collapse">
                                         <div class="box-body">
